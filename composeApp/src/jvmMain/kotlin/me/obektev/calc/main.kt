ce970f616b11581c1a7c05da3eb26d66818dd269
Obektev <Obektev@gmail.com>
2026-03-31 20:23:12 +0300

    chore: initial Compose Multiplatform calculator template

---
+++ composeApp/src/jvmMain/kotlin/me/obektev/calc/main.kt
@@ -0,0 +1,13 @@
+package me.obektev.calc
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Calculator",
+    ) {
+        App()
+    }
+}
