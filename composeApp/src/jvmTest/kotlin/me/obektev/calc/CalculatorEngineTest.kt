24eb2530bb7cd2eea149af6b4dfc65082809b33e
Obektev <Obektev@gmail.com>
2026-03-31 20:32:26 +0300

    test(lab2): add factory and functional button coverage

--- composeApp/src/jvmTest/kotlin/me/obektev/calc/CalculatorEngineTest.kt
+++ composeApp/src/jvmTest/kotlin/me/obektev/calc/CalculatorEngineTest.kt
@@ -55,4 +55,42 @@ class CalculatorEngineTest {
 
         assertEquals("Error", engine.display)
     }
+
+    @Test
+    fun shouldToggleSign() {
+        val engine = CalculatorEngine()
+
+        engine.inputDigit(2)
+        engine.inputDigit(5)
+        engine.toggleSign()
+
+        assertEquals("-25", engine.display)
+    }
+
+    @Test
+    fun shouldApplyPercent() {
+        val engine = CalculatorEngine()
+
+        engine.inputDigit(5)
+        engine.inputDigit(0)
+        engine.applyPercent()
+
+        assertEquals("0.5", engine.display)
+    }
+
+    @Test
+    fun shouldClearEntryOnly() {
+        val engine = CalculatorEngine()
+
+        engine.inputDigit(9)
+        engine.inputDigit(9)
+        engine.setOperation("+")
+        engine.inputDigit(1)
+        engine.inputDigit(2)
+        engine.clearEntry()
+        engine.inputDigit(3)
+        engine.evaluate()
+
+        assertEquals("102", engine.display)
+    }
 }
